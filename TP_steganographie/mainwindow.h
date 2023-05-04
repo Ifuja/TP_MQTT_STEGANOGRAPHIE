9220a87a530034e8185bb468e8c96c70d7324337
Ifuja CHAMOINRI <ifuja.chamoinri@gmail.com>
2023-05-04 17:50:53 +0200

    ajout des tp steganographi et mqtt

---
+++ TP_steganographie/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QTextStream>
+#include <QTextEdit>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QString j_original_filePath;
+    QString j_modified_filePath;
+    QString content_original;
+    QString content_modified;
+    QString differences;
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
---
+++ TestQtMQTT/mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    /***Settings of MQTT broker***/
+    QSettings *ssettings= new QSettings();
+    ssettings->setValue("port",1883);
+    ssettings->setValue("host","broker.emqx.io");
+    ssettings->setValue("qosLevel",0);
+
+    /***Settings of MQTT client***/
+    client= new QMqttClient(this);
+    client->setHostname("broker.emqx.io");
+    client->setPort(1883);
+    client->connectToHost();
+
+    /***The method for published a message on the topic "/ynov/bordeaux/"***/
+    connect(ui->pb_published,&QPushButton::clicked, this, [this]()
+    {
+        client->publish(QMqttTopicName("/ynov/bordeaux/"),ui->enter_message->text().toLocal8Bit());
+        qDebug()<<" state "<< client->state();
+        QMqttSubscription* sub= client->subscribe(QMqttTopicFilter("/ynov/bordeaux/"));
+    });
+
+    /***The method for received a message sended by the MQTT client***/
+    connect(client,&QMqttClient::messageReceived, this, [this](const QByteArray& message,const QMqttTopicName& topic)
+    {
+        QString texte= QString("Topic:")+topic.name();
+        texte+= QString(" message ")+ QString( message);
+        ui->received_message->setText(texte);
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
